bfa5b02147c9b00a6ed23be7b0a4e0a62f34b48d
WensenLi234 <wli9204@bths.edu>
2024-05-21 00:40:15 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const datacenter = document.getElementById("datacenter");
+const pointGrid = document.getElementById("attack_grid");
+const attackPoints = document.getElementsByClassName("attack_point");
+let attackTypes = [
+    "DDOS",
+    "Template"
+]
+
